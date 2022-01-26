2fb543a3fbd113842160a2cdaf11f98131542f92
ppeduzzi <93376026+supergrugno@users.noreply.github.com>
2022-01-26 14:10:11 +0100

    Added final sprites for mg 2,3

--- GameJam 2022 GD/Assets/Scenes/Scripts/MG_2/DirtyPiece.cs
+++ GameJam 2022 GD/Assets/Scenes/Scripts/MG_2/DirtyPiece.cs
@@ -4,6 +4,15 @@ using UnityEngine;
 
 public class DirtyPiece : MonoBehaviour
 {
+    public Sprite[] _sprites;
+    private SpriteRenderer sr;
+
+    private void Start()
+    {
+        sr = gameObject.GetComponent<SpriteRenderer>();
+        sr.sprite = _sprites[Random.Range(0, _sprites.Length)];
+    }
+
     private void OnMouseDown()
     {
         Destroy(gameObject);
