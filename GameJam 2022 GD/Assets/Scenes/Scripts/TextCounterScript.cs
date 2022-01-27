f9f5d427b5d8c9e4af0af3278e196c22e3078bf8
ppeduzzi_laptop <93376026+supergrugno@users.noreply.github.com>
2022-01-27 16:56:29 +0100

    Bug fixes

--- GameJam 2022 GD/Assets/Scenes/Scripts/TextCounterScript.cs
+++ GameJam 2022 GD/Assets/Scenes/Scripts/TextCounterScript.cs
@@ -6,24 +6,13 @@ using TMPro;
 public class TextCounterScript : MonoBehaviour
 {
     //this UI setup shows the amount of monsters captured
-    private string textString;
+    private string textString = "placeholder";
     private TextMeshProUGUI textmeshPro;
 
     private void Start()
     {
         textmeshPro = this.gameObject.GetComponent<TextMeshProUGUI>();
-    }
-
-    private void Update()
-    {
         textString = StaticValues.monsterCount.ToString();
         textmeshPro.SetText(textString);
-
-        //testing keys 
-        //TO REMOVE IN END PRODUCT
-        if (Input.GetKeyDown(KeyCode.E))
-        {
-            StaticValues.monsterCount += 1;
-        }
     }
 }
